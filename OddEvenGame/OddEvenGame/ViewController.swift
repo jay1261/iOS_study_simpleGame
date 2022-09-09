fd23c488d1611a02202d7ccfd4df7df88d5d4c58
jay1261 <dongjae73@naver.com>
2022-09-09 23:00:09 +0900

    Game UI

--- OddEvenGame/OddEvenGame/ViewController.swift
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var computerBallCountLabel: UILabel!
+    
+    @IBOutlet weak var userBallCountLabel: UILabel!
+    var comBallCount: Int = 20
+    var userBallCount: Int = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        computerBallCountLabel.text = String(comBallCount)
+        userBallCountLabel.text = String(userBallCount)
     }
 
 
